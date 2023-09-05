2dc5fb542444770c3990061787a7d9b199e493f8
Amina <amina.begum.01@gmail.com>
2023-09-05 13:23:07 +0100

    Test BankAccount creates multiple new credit transactions when multiple credits added - passes

--- tests/BankAccount.test.js
+++ tests/BankAccount.test.js
@@ -41,4 +41,11 @@ describe("BankAccount", () => {
                                                     balance: 100.00 }])
     })
 
+    it('creates multiple new credit transactions when multiple credits added', () => {
+        bankAccount.makeDeposit(100.00, '01/09/2023')
+        bankAccount.makeDeposit(200.00, '02/09/2023')
+        expect(bankAccount.transactions).toEqual([{ date: '01/09/2023', credit: 100.00, debit: null, balance: 100.00 },
+                                                  { date: '02/09/2023', credit: 200.00, debit: null, balance: 300.00 }])
+    })
+
 })
