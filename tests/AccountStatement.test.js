7d70629612db9f247041bde9ea59c2b8a49c1e39
Amina <amina.begum.01@gmail.com>
2023-09-05 19:24:24 +0100

    Test AccountStatement shows multiple account transactions in a nicely formatted way, most recent first

--- tests/AccountStatement.test.js
+++ tests/AccountStatement.test.js
@@ -25,19 +25,23 @@ describe("AccountStatement", () =>{
             `date || credit || debit || balance\n01/09/2023 || 100.55 || || 100.55`
         )
     })
-//     it('shows multiple account transactions in a nicely formatted way', () => {
-//         const mockBankAccount = { balance : 100.00, 
-//                                   transactions: [{  date: '01/09/2023', 
-//                                                     credit: '100.55', 
-//                                                     debit: null, 
-//                                                     balance: 100.55}]}
+    it('shows multiple account transactions in a nicely formatted way, most recent first', () => {
+        const mockBankAccount = { balance : 100.00, 
+                                  transactions: [{  date: '01/09/2023', 
+                                                    credit: '100.00', 
+                                                    debit: null, 
+                                                    balance: 100.00},
+                                                    {  date: '02/09/2023', 
+                                                    credit: '200.00', 
+                                                    debit: null, 
+                                                    balance: 300.00}
+                                                ]}
         
-//         const accountStatement = new AccountStatement(mockBankAccount)
-//         const statement = accountStatement.printStatement()
+        const accountStatement = new AccountStatement(mockBankAccount)
+        const statement = accountStatement.printStatement()
                                                     
-//         expect(statement).toEqual(
-//             `date || credit || debit || balance
-// 01/09/2023 || 100.55 || || 100.55`
-//         )
-//     })
+        expect(statement).toEqual(
+            `date || credit || debit || balance\n02/09/2023 || 200.00 || || 300.00\n01/09/2023 || 100.00 || || 100.00`
+        )
+    })
 })
