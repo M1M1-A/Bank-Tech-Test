d39420b4266f387392892509a8a5a18627467f5b
Amina <amina.begum.01@gmail.com>
2023-09-05 13:10:26 +0100

    Test BankAccount adds a credit amount to the balance - passes

--- tests/BankAccount.test.js
+++ tests/BankAccount.test.js
@@ -17,8 +17,13 @@ describe("BankAccount", () => {
         expect(bankAccount.balance).toBe(0);
     })
 
-    it('shows transactions as empty array', () => {
+    it('initially shows transactions as empty array', () => {
         expect(bankAccount.transactions).toEqual([])
+    })
+
+    it('adds a credit amount to the balance', () => {
+        bankAccount.makeDeposit(1000.00, '01/09/2023')
+        expect(bankAccount.balance).toBe(1000.00)
     })
 
 })
