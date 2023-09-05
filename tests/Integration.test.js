7c38fdd08d2d1c35e3665b2e9b9bf839511d0278
Amina <amina.begum.01@gmail.com>
2023-09-05 21:35:16 +0100

    Test BankAccount & AccountStatement creates a new bank account, makes a multiple deposits and withdrawals and these are reflected in statement with most recent first - passes

--- tests/Integration.test.js
+++ tests/Integration.test.js
@@ -36,4 +36,21 @@ describe("BankAccount and AccountStatement", () => {
             `date || credit || debit || balance\n02/09/2023 || || 50.50 || 49.50\n01/09/2023 || 100.00 || || 100.00`
         )
     })
+
+    it('creates a new bank account, makes a multiple deposits and withdrawals and these are reflected in statement with most recent first', () => {
+        bankAccount.makeDeposit(200.00, '01/09/2023')
+        bankAccount.makeWithdrawal(100.00, '02/09/2023')
+        bankAccount.makeDeposit(200.00, '03/09/2023')
+        bankAccount.makeWithdrawal(100.00, '04/09/2023')
+
+        const statement = accountStatement.printStatement()
+
+        expect(statement).toEqual(
+            `date || credit || debit || balance
+04/09/2023 || || 100.00 || 200.00
+03/09/2023 || 200.00 || || 300.00
+02/09/2023 || || 100.00 || 100.00
+01/09/2023 || 200.00 || || 200.00`
+        )
+    })
 })
