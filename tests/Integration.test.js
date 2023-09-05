5b2a9c93c5c1fe994e27dcdac1006ac432aabd3d
Amina <amina.begum.01@gmail.com>
2023-09-05 19:45:43 +0100

    Test Integration creates a new bank account, makes a withdrawal and statement reflects this as debit - passes

--- tests/Integration.test.js
+++ tests/Integration.test.js
@@ -25,4 +25,15 @@ describe("BankAccount and AccountStatement", () => {
             `date || credit || debit || balance\n01/09/2023 || 100.00 || || 100.00`
         )
     })
+
+    it('creates a new bank account, makes a withdrawal and statement reflects this as debit', () => {
+        
+        bankAccount.makeDeposit(100.00, '01/09/2023')
+        bankAccount.makeWithdrawal(50.50, '02/09/2023')
+        const statement = accountStatement.printStatement()
+
+        expect(statement).toEqual(
+            `date || credit || debit || balance\n02/09/2023 || || 50.50 || 49.50\n01/09/2023 || 100.00 || || 100.00`
+        )
+    })
 })
