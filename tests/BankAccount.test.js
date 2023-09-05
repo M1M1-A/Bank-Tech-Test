2eea6979562537afefd03db1989b25088d55c078
Amina <amina.begum.01@gmail.com>
2023-09-05 13:05:04 +0100

    Test BankAccount shows an initial balance of zero - passes

---
+++ tests/BankAccount.test.js
@@ -0,0 +1,20 @@
+const BankAccount = require("../lib/BankAccount")
+
+describe("BankAccount", () => {
+
+    let bankAccount;
+
+    beforeAll(() => {
+        bankAccount = new BankAccount();
+    })
+
+    afterEach(() => {
+        bankAccount.balance = 0;
+        bankAccount.transactions = [];
+    })
+
+    it("shows an initial balance of zero", () => {
+        expect(bankAccount.balance).toBe(0);
+    })
+
+})
